a1fc9f844e008983e09f1155a8dcebef5e290a1b
fontisawesome <fontisawesome@gmail.com>
2019-05-20 13:13:00 -0400

    Physics Engine is now been swapped to Box2d. Working on graphics.

--- core/src/com/mygdx/game/ISPTesting.java
+++ core/src/com/mygdx/game/ISPTesting.java
@@ -3,126 +3,95 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.*;
+import com.sun.org.apache.xpath.internal.operations.Or;
 
 public class ISPTesting extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture grass;
-	Texture slime;
-	int slimeX;
-	int slimeY;
-	int stateOfSlime;
-	int speedOfSlime;
-	int acceleration;
-	int ground;
-	int lastSlimeY;
-	Terrain t;
-	OrthographicCamera camera;
-	boolean forward;
+	private World world;
+	private Box2DDebugRenderer debug;
+	private OrthographicCamera camera;
+	private Body slime;
+	private Body ground;
+	private float maximumVelocity;
+	@Override
+	public void resize(int width, int height){
+		camera.viewportHeight = height/40;
+		camera.viewportWidth = width/40;
+		camera.update();
+	}
 	@Override
 	public void create () {
-		t = new Terrain();
-		ground = (int)t.uheights[0]+32;
-		batch = new SpriteBatch();
-		grass = new Texture("pixel.png");
-		slime = new Texture("slime.png");
-		slimeX = 0;
-		slimeY = ground;
-		stateOfSlime = 0;
-		speedOfSlime = 0;
-		acceleration = 2;
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-		forward = true;
+		maximumVelocity = 10;
+		world = new World(new Vector2(0, -10), true);
+		debug = new Box2DDebugRenderer();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth()/40, Gdx.graphics.getHeight()/40);
+
+		//body creations
+		BodyDef slimeDef = new BodyDef();
+		slimeDef.type = BodyDef.BodyType.DynamicBody;
+		slimeDef.position.set(0,1);
+		BodyDef groundDef = new BodyDef();
+		groundDef.type = BodyDef.BodyType.StaticBody;
+		groundDef.position.set(0,0);
+
+		//slime shape
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(0.5f,0.5f);
+		ChainShape groundShape = new ChainShape();
+		groundShape.createChain(new Vector2[] {new Vector2(-500,0), new Vector2(500,0)});
+
+
+		//fixture creations
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.density = 1f;
+		fixtureDef.friction = 0.33f;
+		fixtureDef.restitution = 0f;
+		slime = world.createBody(slimeDef);
+		Fixture slimeFix = slime.createFixture(fixtureDef);
+		FixtureDef groundFixDef = new FixtureDef();
+		groundFixDef.shape = groundShape;
+		groundFixDef.friction = 0.5f;
+		groundFixDef.restitution = 0;
+		ground = world.createBody(groundDef);
+		Fixture groundFix = ground.createFixture(groundFixDef);
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0.5f, 0, 1);
+		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		for (int i = 0; i < 800; i+= 32) {
-			for (int y = (int)t.uheights[i/32]; y >= 0; y-=32)
-				batch.draw(grass, i, y);
+		debug.render(world, camera.combined);
+		if (Gdx.input.isKeyPressed(Input.Keys.D) && slime.getLinearVelocity().x < maximumVelocity) {
+				slime.applyLinearImpulse(2,0, slime.getPosition().x, slime.getPosition().y, true);
 		}
-		batch.draw(slime, slimeX, slimeY,32,32);
-		batch.end();
-		double leftHeight = t.uheights[slimeX/32];
-		double rightHeight = t.uheights[Math.min(slimeX/32 + 1, t.uheights.length-1)];
-		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-			forward = false;
-			if (slimeY > Math.max(leftHeight, rightHeight)+32 && stateOfSlime == 0){
-				stateOfSlime = -1;
-			}
-			if (slimeY-32 >= leftHeight) {
-				slimeX -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.A)&& slime.getLinearVelocity().x > -1*maximumVelocity) {
+			slime.applyLinearImpulse(-2,0, slime.getPosition().x, slime.getPosition().y, true);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			if (Math.abs(slime.getPosition().y-0.5f) < 0.02){
+				slime.applyLinearImpulse(0,6, slime.getPosition().x, slime.getPosition().y, true);
 			}
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-			forward = true;
-			System.out.println("left: " + slimeY + " right: " + rightHeight);
-			if (slimeY > Math.max(leftHeight, rightHeight)+32 && stateOfSlime == 0){
-				stateOfSlime = -1;
-			}
-			if (slimeY-32 >= rightHeight) {
-				slimeX += 200 * Gdx.graphics.getDeltaTime();
-			}
-
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.W)){
-			if (stateOfSlime == 0) {
-				stateOfSlime = 1;
-				speedOfSlime = 16;
-			}
-		}
-		if (stateOfSlime == 1){
-			slimeY += speedOfSlime;
-			speedOfSlime-= acceleration;
-			if (speedOfSlime <= 0){
-				stateOfSlime = -1;
-				speedOfSlime = 0;
-			}
-		}
-		else if(stateOfSlime == -1){
-			slimeY -= speedOfSlime;
-			speedOfSlime += acceleration;
-			if (forward) {
-				if (slimeY <= (int) Math.max(leftHeight, rightHeight) + 32 && lastSlimeY > (int) Math.max(leftHeight, rightHeight) + 32) {
-					stateOfSlime = 0;
-					speedOfSlime = 0;
-					slimeY = (int) Math.max(leftHeight, rightHeight) + 32;
-				} else if (lastSlimeY <= (int) Math.max(leftHeight, rightHeight) + 32 && slimeY <= leftHeight + 32) {
-					stateOfSlime = 0;
-					speedOfSlime = 0;
-					slimeY = (int) leftHeight + 32;
-					slimeX = (slimeX / 32) * 32;
-				}
-			}
-			else{
-				if (slimeY <= (int) Math.max(leftHeight, rightHeight) + 32 && lastSlimeY > (int) Math.max(leftHeight, rightHeight) + 32) {
-					stateOfSlime = 0;
-					speedOfSlime = 0;
-					slimeY = (int) Math.max(leftHeight, rightHeight) + 32;
-				} else if (lastSlimeY <= (int) Math.max(leftHeight, rightHeight) + 32 && slimeY <= rightHeight + 32) {
-					stateOfSlime = 0;
-					speedOfSlime = 0;
-					slimeY = (int) rightHeight + 32;
-					slimeX = (slimeX / 32 + 1) * 32;
-				}
-			}
-			lastSlimeY = slimeY;
-		}
+		System.out.println(slime.getPosition().x + " " + slime.getPosition().y);
+		world.step(1/60f, 8, 3);
 	}
-
+	public void hide(){
+		dispose();
+	}
 	@Override
 	public void dispose () {
-		batch.dispose();
-		grass.dispose();
-		slime.dispose();
+		world.dispose();
+		debug.dispose();
 	}
 }
